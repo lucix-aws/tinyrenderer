dbb33f2ea956e851cd24ae59410903a166114a4e
Luc Talatinian <lucix@amazon.com>
2025-05-12 09:56:47 -0400

    it draws a pixel

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+const width = 800
+const height = 800
+
+var red = color.RGBA{
+	0xff, 0, 0, 0xff,
+}
 
 func main() {
-	fmt.Println("vim-go")
+	img := image.NewRGBA(image.Rectangle{
+		Max: image.Point{width, height},
+	})
+
+	img.Set(400, 400, red)
+
+	f, err := os.Create("out.png")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	if err := png.Encode(f, img); err != nil {
+		panic(err)
+	}
 }
