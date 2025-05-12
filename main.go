bcbabc882008314c28d6e341e21657a7e63183fd
Luc Talatinian <lucix@amazon.com>
2025-05-12 13:40:13 -0400

    previous commit was actually wrong because my stepY was wrong

--- main.go
+++ main.go
@@ -25,14 +25,21 @@ var (
 // instead of an arbitrary step, use dx to figure out how many pixels to draw
 // creep along the y axis fractionally as we do so
 //
-// this does NOT work for lines w/ slope > 45deg because dx does not correspond
+// this does NOT work for lines where dy > dx because dx does not correspond
 // to number of iterations in that case
 func line(img *image.RGBA, x0, y0, x1, y1 int, c color.Color) {
-	dy := float64(y1 - y0)
-	dx := float64(x1 - x0)
-	for i := x0; i <= x1; i++ {
-		sy := dy*float64(i)/dx - dy
-		img.Set(i, y0+int(sy), c)
+	dy := y1 - y0
+	dx := x1 - x0
+	for i := 0; i <= dx; i++ {
+		sy := float64(dy) * float64(i) / float64(dx)
+		img.Set(x0+i, y0+int(sy), c)
+	}
+}
+
+// stopgap before vertical lines actually work
+func vline(img *image.RGBA, x int, c color.Color) {
+	for i := 0; i < img.Rect.Max.Y; i++ {
+		img.Set(x, i, c)
 	}
 }
 
@@ -44,12 +51,17 @@ func render(i *image.RGBA) {
 	line(i, 0, 500, 800, 500, white)
 	line(i, 0, 600, 800, 600, white)
 	line(i, 0, 700, 800, 700, white)
-	line(i, 200, 200, 400, 100, cyan)
+	vline(i, 100, white)
+	vline(i, 200, white)
+	vline(i, 300, white)
+	vline(i, 400, white)
+	vline(i, 500, white)
+	vline(i, 600, white)
+	vline(i, 700, white)
 
-	// should not work? because dx is 0
-	line(i, 200, 200, 200, 100, magenta)
-	// should not work? because dx != iterations
-	line(i, 200, 200, 250, 300, yellow)
+	line(i, 200, 200, 700, 100, red)
+	// not correct (dotted line) bc dy > dx
+	line(i, 200, 200, 300, 700, cyan)
 }
 
 func main() {
